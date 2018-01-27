04c96c20f4654b638476f282cb9a1f21a5e8bc3e
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2018-01-27 12:58:12 -0200

    survivors-fixture.js created to create survivor data automated

---
+++ test/utils/fixtures/survivors/survivors-fixture.js
@@ -0,0 +1,37 @@
+const Chance = require('chance');
+
+const chance = new Chance();
+
+function getInventory() {
+  return {
+    water: chance.natural({ min: 0, max: 10 }),
+    food: chance.natural({ min: 0, max: 10 }),
+    medication: chance.natural({ min: 0, max: 10 }),
+    ammunition: chance.natural({ min: 0, max: 10 })
+  };
+}
+
+function getLocation() {
+  return {
+    latitude: chance.latitude(),
+    longitude: chance.longitude()
+  };
+}
+
+function create(data = {}) {
+  const survivor = {};
+  
+  survivor.name = data.name || chance.name();
+  survivor.age = data.age || chance.age();
+  survivor.gender = data.gender || chance.gender();
+  survivor.location = data.location || getLocation();
+  survivor.inventory = data.inventory || getInventory();
+  survivor.infected = data.infected || chance.bool();
+  survivor.numInfectedNotification = data.numInfectedNotification || chance.natural({ min: 0, max: 3 });
+  
+  return survivor;
+}
+
+module.exports = {
+  create
+};
